d8c8e18a0f0545d1089feb76a9118ab464cb0d88
Niraj Menon <nirajmmenon@gmail.com>
2024-02-13 14:06:28 -0500

    lab7 finalization

---
+++ code/src/main.c
@@ -0,0 +1,159 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Weili An, Niraj Menon
+  * @date    Feb 7, 2024
+  * @brief   ECE 362 Lab 7 student template
+  ******************************************************************************
+*/
+
+/*******************************************************************************/
+
+// Fill out your username!  Even though we're not using an autotest, 
+// it should be a habit to fill out your username in this field now.
+const char* username = "username";
+
+/*******************************************************************************/ 
+
+#include "stm32f0xx.h"
+#include <stdint.h>
+
+void internal_clock();
+
+// Uncomment only one of the following to test each step
+#define STEP1
+// #define STEP2
+// #define STEP3
+// #define STEP4
+
+void init_usart5() {
+    // TODO
+}
+
+#ifdef STEP1
+int main(void){
+    internal_clock();
+    init_usart5();
+    for(;;) {
+        while (!(USART5->ISR & USART_ISR_RXNE)) { }
+        char c = USART5->RDR;
+        while(!(USART5->ISR & USART_ISR_TXE)) { }
+        USART5->TDR = c;
+    }
+}
+#endif
+
+#ifdef STEP2
+#include <stdio.h>
+
+// TODO Resolve the echo and carriage-return problem
+
+int __io_putchar(int c) {
+    // TODO
+    while(!(USART5->ISR & USART_ISR_TXE));
+    USART5->TDR = c;
+    return c;
+}
+
+int __io_getchar(void) {
+    while (!(USART5->ISR & USART_ISR_RXNE));
+    char c = USART5->RDR;
+    // TODO
+    return c;
+}
+
+int main() {
+    internal_clock();
+    init_usart5();
+    setbuf(stdin,0);
+    setbuf(stdout,0);
+    setbuf(stderr,0);
+    printf("Enter your name: ");
+    char name[80];
+    fgets(name, 80, stdin);
+    printf("Your name is %s", name);
+    printf("Type any characters.\n");
+    for(;;) {
+        char c = getchar();
+        putchar(c);
+    }
+}
+#endif
+
+#ifdef STEP3
+#include <stdio.h>
+#include "fifo.h"
+#include "tty.h"
+int __io_putchar(int c) {
+    // TODO Copy from your STEP2
+}
+
+int __io_getchar(void) {
+    // TODO
+}
+
+int main() {
+    internal_clock();
+    init_usart5();
+    setbuf(stdin,0);
+    setbuf(stdout,0);
+    setbuf(stderr,0);
+    printf("Enter your name: ");
+    char name[80];
+    fgets(name, 80, stdin);
+    printf("Your name is %s", name);
+    printf("Type any characters.\n");
+    for(;;) {
+        char c = getchar();
+        putchar(c);
+    }
+}
+#endif
+
+#ifdef STEP4
+
+#include <stdio.h>
+#include "fifo.h"
+#include "tty.h"
+
+// TODO DMA data structures
+
+void enable_tty_interrupt(void) {
+    // TODO
+}
+
+// Works like line_buffer_getchar(), but does not check or clear ORE nor wait on new characters in USART
+char interrupt_getchar() {
+    // TODO
+}
+
+int __io_putchar(int c) {
+    // TODO copy from STEP2
+}
+
+int __io_getchar(void) {
+    // TODO Use interrupt_getchar() instead of line_buffer_getchar()
+}
+
+// TODO Copy the content for the USART5 ISR here
+// TODO Remember to look up for the proper name of the ISR function
+
+int main() {
+    internal_clock();
+    init_usart5();
+    enable_tty_interrupt();
+
+    setbuf(stdin,0); // These turn off buffering; more efficient, but makes it hard to explain why first 1023 characters not dispalyed
+    setbuf(stdout,0);
+    setbuf(stderr,0);
+    printf("Enter your name: "); // Types name but shouldn't echo the characters; USE CTRL-J to finish
+    char name[80];
+    fgets(name, 80, stdin);
+    printf("Your name is %s", name);
+    printf("Type any characters.\n"); // After, will type TWO instead of ONE
+    for(;;) {
+        char c = getchar();
+        putchar(c);
+    }
+}
+#endif
