dcc6e092d7739ab4705621d2aa47deeca6b552e9
Jack Warren MacDougall <jwmacdou@ecelnx73.ecn.purdue.edu>
2024-10-15 15:09:28 -0400

    Lab 7 part 1

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 
 // Fill out your username!  Even though we're not using an autotest, 
 // it should be a habit to fill out your username in this field now.
-const char* username = "username";
+const char* username = "jwmacdou";
 
 /*******************************************************************************/ 
 
@@ -28,6 +28,43 @@ void internal_clock();
 
 void init_usart5() {
     // TODO
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    RCC->AHBENR |= RCC_AHBENR_GPIODEN;
+    //configure pin PC12 to be routed to USART5_TX ?
+    //set moder to alternate function then AFR to correct value
+    GPIOC->MODER &= ~(0b11<<24);
+    GPIOC->MODER |= 0b10<<24;
+    GPIOC->AFR[1] &= ~(0b1111<<16);
+    GPIOC->AFR[1] |= 0b1<<17;
+    //configure pin PD2 to be routed to USART5_RX ?
+    GPIOD->MODER &= ~(0b11<<4);
+    GPIOD->MODER |= 0b10<<4;
+    GPIOD->AFR[0] &= ~(0b1111<<8);
+    GPIOD->AFR[0] |= 0b1<<9;
+    RCC->APB1ENR |= RCC_APB1ENR_USART5EN;
+    //Disable USART5 by turning off its UE bit v
+    USART5->CR1 &= ~0b1;
+    //Set a word size of 8 bits v
+    USART5->CR1 &= ~(0b1<<28);
+    USART5->CR1 &= ~(0b1<<12);
+    //set it for one stop bit (Bit 28 of USART5->CR1, how do you access "n" value for "n stop bits"?) ?
+    USART5->CR2 &= ~(0b11<<12);
+    //set it for no parity control v
+    USART5->CR1 &= ~(0b1<<10);
+    //Use 16x oversampling v
+    USART5->CR1 &= ~(0b1<<15);
+    //Set baud rate to 115.2 KBps (Serial no. 7 on Table 96) v?
+    USART5->BRR = 0x1A1;
+    //Enable the transmitter and the receiver by setting the TE and RE bits v
+    USART5->CR1 |= 0b11<<2;
+    //Enable the USART v
+    USART5->CR1 |= 0b1;
+    //wait for TE and RE bits to be acknowledged by checking TEACK and REACK are both set in the ISR. v ?
+    while (!(USART5->ISR & USART_ISR_TEACK) && !(USART5->ISR & USART_ISR_REACK)){
+
+    }
+    
+    
 }
 
 #ifdef STEP1
@@ -50,6 +87,13 @@ int main(void){
 
 int __io_putchar(int c) {
     // TODO
+    if (c == '\n'){
+        //if the character passes is a \n first write a \r to USART5->TDR v?
+        while (~USART_ISR_TXE){
+
+        }
+        USART5->TDR = '\r';
+    }
     while(!(USART5->ISR & USART_ISR_TXE));
     USART5->TDR = c;
     return c;
@@ -59,6 +103,15 @@ int __io_getchar(void) {
     while (!(USART5->ISR & USART_ISR_RXNE));
     char c = USART5->RDR;
     // TODO
+    //if the character read c is a carriage return ('\r'), change it to ('\n') v?
+    if (c == '\r'){
+        while (~USART_ISR_TXE){
+
+        }
+        USART5->TDR = '\n';
+    }
+    //echo the character read to the output by calling _io_putchar(c) before it's returned
+    __io_putchar(c);
     return c;
 }
 
@@ -90,6 +143,7 @@ int __io_putchar(int c) {
 
 int __io_getchar(void) {
     // TODO
+    return line_buffer_getchar();
 }
 
 int main() {
@@ -117,9 +171,21 @@ int main() {
 #include "tty.h"
 
 // TODO DMA data structures
+#define FIFOSIZE 16
+char serfifo [FIFOSIZE];
+int seroffset = 0;
 
 void enable_tty_interrupt(void) {
     // TODO
+    //raise an interrupt every time the receive data register becomes not empty
+    //trigger a DMA operation every time the receive data register becomes not empty. Do this by enabling DMA mode for reception
+    //both of these ^ are flag updates in the USART CR1 and CR3 (and NVIC ISER).
+    //Enable RCC clock for DMA controller 2
+    //Configure DMA 2_channel 2 for the following:
+    //CMAR should be set to the address of serfifo
+    //CPAR should be set to the address of USART5->RDR
+    //CNDTR should be set to FIFOSIZE
+
 }
 
 // Works like line_buffer_getchar(), but does not check or clear ORE nor wait on new characters in USART
